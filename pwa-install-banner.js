97488434b81773a559165d629adf6606167a535d
kelvinl96 <kelvin_lee-96@hotmail.com>
2026-01-26 14:41:57 +0800

    Update: Set manifestUrl to hosted CDN link

--- pwa-install-banner.js
+++ pwa-install-banner.js
@@ -21,7 +21,7 @@
         iosInstructions: 'Tap the share button <img src="data:image/svg+xml,%3Csvg xmlns=\'http://www.w3.org/2000/svg\' width=\'16\' height=\'16\' viewBox=\'0 0 24 24\'%3E%3Cpath fill=\'%23000\' d=\'M18 16.08c-.76 0-1.44.3-1.96.77L8.91 12.7c.05-.23.09-.46.09-.7s-.04-.47-.09-.7l7.05-4.11c.54.5 1.25.81 2.04.81 1.66 0 3-1.34 3-3s-1.34-3-3-3-3 1.34-3 3c0 .24.04.47.09.7L8.04 9.81C7.5 9.31 6.79 9 6 9c-1.66 0-3 1.34-3 3s1.34 3 3 3c.79 0 1.5-.31 2.04-.81l7.12 4.16c-.05.21-.08.43-.08.65 0 1.61 1.31 2.92 2.92 2.92 1.61 0 2.92-1.31 2.92-2.92s-1.31-2.92-2.92-2.92z\'/%3E%3C/svg%3E" style="width:16px;height:16px;vertical-align:middle;"> and select "Add to Home Screen"',
         autoTriggerInstall: false, // Set to true to auto-show install prompt after banner appears
         autoTriggerDelay: 2000, // Delay in milliseconds before auto-triggering (2000 = 2 seconds)
-        manifestUrl: null, // Set to your hosted manifest URL if you want to use a hosted manifest instead of dynamic
+        manifestUrl: 'https://cdn.jsdelivr.net/gh/kelvinl961/script-testing@main/manifest.json', // Hosted manifest URL
         injectManifest: true, // Set to true to inject manifest for third-party sites
     };
 
